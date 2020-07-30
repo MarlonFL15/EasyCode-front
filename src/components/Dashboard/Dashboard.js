fe9ca67f45eee8e909ed6e4c90c7e6e61cf343c3
Monike Freitas <monike.ftsousa@gmail.com>
2020-07-30 13:11:24 -0400

    Base da tela de dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,77 @@
 import React from 'react'
 import Blockly from '../Questions/Blockly'
+import { Grid, Divider, Typography } from '@material-ui/core'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import colors from '../Colors'
 
-import ReactDOM from 'react-dom';
-
+const Text = withStyles({
+    root: {
+        fontFamily: 'Quicksand, sans-serif'
+    },
+  })(Typography);
+  
+const useStyles = makeStyles((theme)=>({
+    top: {
+        height: '50%',
+        minHeight: 280,
+        backgroundColor: colors.blue,
+        borderRadius: 10,
+        padding: 15,
+        margin: 6,
+        color: '#fff'
+    },
+    bottom: {
+        backgroundColor: colors.blue,
+        height: '30%',
+        minHeight: 180,
+        borderRadius: 10,
+        margin: 6,
+    }
+}))
 export default props =>{
-    console.log(<Blockly/>)
+    const classes = useStyles()
+     
     return(
-        /*
-        <Roullete items={['Se','Faça','Estruturas alinhadas','Enquanto', 'Repita', 'Escolha-caso']}/>
-        */
-       <div>Dashboard do perfil</div>
+        <Grid container style={{padding: 7}}>
+            <Grid item sm={4}>
+                <div className={classes.top}>
+                    <Text variant="h6" style={{textAlign: 'center'}}><b>Meu perfil</b></Text>
+                </div>
+            </Grid>
+            <Grid item sm={8}>
+                <div className={classes.top}>
+                    <Text variant="h6" style={{textAlign: 'center'}}><b>Conquistas</b></Text>
+                </div>
+            </Grid>
+            <Grid container sm={12} spacing={1} 
+            style={{marginTop: 10, padding: '0 5px', marginBottom: 10}}>
+                <Grid item sm={12}>
+                    <Divider light />
+                </Grid>  
+                <Grid item sm={12}
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                }}>
+                    <Text variant="h6"><b>Histórico de atividades</b></Text>
+                    <Text variant="h6" > veja mais</Text>
+                </Grid>
+            </Grid>
+            <Grid item sm={4}>
+                <div className={classes.bottom}>
+                    
+                </div>
+            </Grid>
+            <Grid item sm={4}>
+                <div className={classes.bottom}>
+                    
+                </div>
+            </Grid>
+            <Grid item sm={4}>
+                <div className={classes.bottom}>
+                    
+                </div>
+            </Grid>
+        </Grid>
     )
 }
